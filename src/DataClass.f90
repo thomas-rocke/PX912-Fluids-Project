9b5012bc5f2b30a7859238df2dec395a9d2e6750
Thomas Rocke <45517493+thomas-rocke@users.noreply.github.com>
2021-12-29 15:37:01 +0000

    NewtonIter done

--- src/DataClass.f90
+++ src/DataClass.f90
@@ -6,7 +6,7 @@ module DataClass
     implicit none
 
     type :: Data
-        real(kind=REAL64), dimension(:, :), allocatable :: h
+        real(kind=REAL64), dimension(:), allocatable :: h
         real(kind=REAL64) :: Rho, g, sigma
         contains
         procedure :: init
@@ -15,17 +15,16 @@ module DataClass
     contains
 
     subroutine init(this, Rho, g, sigma, h_map)
-        ! Initialize Data type with a 2D h_map
-        ! h = h_map(x, y) or h = h_map(r, theta)
+        ! Initialize Data type with a h_map
         class(Data), intent(out) :: this
         real(kind=REAL64), intent(in) :: Rho, g, sigma
-        real(kind=REAL64), dimension(:, :), intent(in)  :: h_map
+        real(kind=REAL64), dimension(:), intent(in)  :: h_map
 
         this%Rho = Rho
         this%g = g
         this%sigma = sigma
 
-        allocate(this%h(size(h_map, 1), size(h_map, 2)))
+        allocate(this%h(size(h_map)))
         this%h = h_map
     end subroutine
 end module DataClass
--- src/NewtonIteration.f90
+++ src/NewtonIteration.f90
@@ -2,13 +2,17 @@
 
 module NewtonIteration
     use ISO_FORTRAN_ENV
+    use DataClass
+    use lapack
     implicit none
 
     interface template
-            pure function func_template(x, N)
+            pure function func_template(x, N, Dat)
                 use ISO_FORTRAN_ENV
+                use DataClass
                 real(kind=REAL64), intent(in), dimension(N) :: x
                 integer, intent(in) :: N
+                type(Data), intent(in) :: Dat
                 real(kind=REAL64), dimension(N) :: func
             end function
     end interface
@@ -16,20 +20,36 @@ module NewtonIteration
 
     contains
     
-    subroutine NewtonIter(x, f, eps, N)
+    subroutine NewtonIter(x, f, eps, N, Dat)
         ! Perform one iteration of the newton method, updating x
         real(kind=REAL64), intent(inout), dimension(N) :: x ! X data
         procedure(func_template), pointer :: f ! Function pointer (Newton error function), returns array of length M
         real(kind=REAL64), intent(in) :: eps ! Deviation used to approx Jacobian
         integer, intent(in) :: N ! Length of x and of f(x)
+        type(Data), intent(in) :: Dat
 
         real(kind=REAL64), dimension(N, N) :: Jacobian
+        real(kind=REAL64), dimension(N) :: dx
+        integer :: ipiv
+
+        Jacobian = GetJacobian(x, f, eps, N, Dat)
+
+        ! Solve linear equation J_ij(x) dx_i = - f_j(x) 
+        dx = - f(x, N, Dat) ! dgesv will modify this to become the correct dx
+        call dgesv(N, 1, Jacobian, N, ipiv, dx, N, info)
+
+        if (info /= 0) then
+            print *, "Error occurred in linear solving" 
+            stop
+        end if
+
+        x = x + dx
 
     end subroutine
 
 
 
-    function GetJacobian(x, f, eps, N) result(Jacobian)
+    function GetJacobian(x, f, eps, N, Dat) result(Jacobian)
         ! Approximates the jacobian at x using a central differences method
         ! Partial Derivative approximation (df_i/dx_j) uses dx_j = eps*x_k j=k, 0 otherwise
 
@@ -37,6 +57,7 @@ module NewtonIteration
         procedure(func_template), pointer :: f ! Function pointer (Newton error function), returns array of length M
         real(kind=REAL64), intent(in) :: eps ! Deviation used to approx Jacobian
         integer, intent(in) :: N ! Length of x and of f(x)
+        type(Data), intent(in) :: Dat
 
         real(kind=REAL64), dimension(N, N) :: Jacobian
         real(kind=REAL64), dimension(N) :: dx
@@ -52,7 +73,7 @@ module NewtonIteration
         DO j=1, N
             dx = 0.0_REAL64
             dx(j) = x(j) * eps ! dx_i = eps * x_j for i=j, 0 otherwise
-            Jacobian(:, j) = (f(x + dx, N) + f(x - dx, N))/(2 * dx(j)) ! Do df_i/dx_j for all f_is 
+            Jacobian(:, j) = (f(x + dx, N, Dat) + f(x - dx, N, Dat))/(2 * dx(j)) ! Do df_i/dx_j for all f_is 
         end do
     end function
 
